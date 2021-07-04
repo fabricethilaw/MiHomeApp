42e9bb67ff40526cbf5ed03453cc0da99f4d7abc
thilawfabrice <fabricethilaw@gmail.co>
2021-07-04 14:51:49 +0100

    login -> handles for clicks on buttons

--- app/src/main/java/comfabricethilaw/smarthomeapp/LoginActivity.kt
+++ app/src/main/java/comfabricethilaw/smarthomeapp/LoginActivity.kt
@@ -2,6 +2,7 @@ package comfabricethilaw.smarthomeapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import comfabricethilaw.smarthomeapp.databinding.ActivityLoginBinding
 
@@ -11,17 +12,55 @@ class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
 
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.btnSignIn.setOnClickListener {
-            startActivity(Intent(this, MainActivity::class.java))
+            handleSignInClick()
         }
-
-        supportActionBar?.hide()
+        binding.btnNewResident.setOnClickListener {
+            handleSignUpClick()
+        }
+        binding.btnHelpPasswordForgot.setOnClickListener {
+            handleForgotPasswordClick()
+        }
 
     }
 
+    private fun handleSignInClick() {
+        val username = binding.inputUsername.text.toString()
+        val password = binding.inputPassword.text.toString()
+
+        validateInput(username, password) { validUsername, validPassword ->
+            submit(username = validUsername, password = validPassword)
+        }
+    }
+
+    private fun handleSignUpClick() {
+        // add your logic
+        Toast.makeText(this, "Todo : add the logic for it", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun handleForgotPasswordClick() {
+        // add your logic
+        Toast.makeText(this, "Todo : add the logic for it", Toast.LENGTH_SHORT).show()
+    }
+
+
+    private fun validateInput(
+        username: String,
+        password: String,
+        onValidationPassed: (String, String) -> Unit
+    ) {
+        // add your validation logic
+        onValidationPassed.invoke(username, password)
+    }
+
+    private fun submit(username: String, password: String) {
+        // add your logic
+        startActivity(Intent(this, MainActivity::class.java))
+    }
 
 }
